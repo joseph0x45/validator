30de35cb0e5921439c9612b36b1865038f7e4495
TheWisePigeon <zozozozeph@gmail.com>
2022-08-27 23:56:05 +0000

    arch: setting up architecture

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
-console.log('Duh', 'ok')
+
+const textInput = document.querySelector("input[type='text'][id='test']") as HTMLInputElement
+let validationClass = textInput.getAttribute('vtype')
+let test = '5'
+console.log(typeof parseInt(test));
+
+
+if (validationClass=='alpha') {
+    let value = textInput.value as string
+    console.log(value);
+    
+    if (typeof parseInt(value)=='number') {
+        console.log('duh');
+        
+    }
+}
---
+++ src/modules/TextInput.ts
@@ -0,0 +1,2 @@
+//Should tell the requested pattern of the value when user focus 
+//Should outline input in red when user entered value doesn't match pattern
