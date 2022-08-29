f4755f7d816a45385d101fd76e11a18e5e27eadc
TheWisePigeon <zozozozeph@gmail.com>
2022-08-29 21:14:07 +0000

    thanosnap coming

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,8 @@
+import { TextInputHandler } from "./modules/TextInput"
 
 const textInput = document.querySelector("input[type='text'][id='test']") as HTMLInputElement
 let validationClass = textInput.getAttribute('vtype')
 
 textInput.addEventListener('input', (event)=>{
-    let target = event.target as HTMLInputElement
-    let value = target.value
-    
-    if (Number.isInteger(Number(value))){
-        console.log('yes');
-        
-    }else{
-        console.log('nope');
-        
-    }
-     
-    
-    
-    
+    TextInputHandler(event)
 })
--- src/modules/TextInput.ts
+++ src/modules/TextInput.ts
@@ -1,2 +1,15 @@
 //Should tell the requested pattern of the value when user focus 
 //Should outline input in red when user entered value doesn't match pattern
+export const TextInputHandler  = (event: Event)=>{
+    const target = event.target as HTMLInputElement
+    let value = target.value
+    
+    if (Number.isInteger(Number(value))){
+        target.style.outlineColor = 'black'
+        console.log('yes');
+        
+    }else{
+        target.style.cssText = "color: blue; outline-color:red"
+    }
+}
+
