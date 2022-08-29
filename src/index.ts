fbc903103b630d48ee9b98a094eaed3ec3eaf390
TheWisePigeon <zozozozeph@gmail.com>
2022-08-29 21:14:59 +0000

    thanosnap

--- src/index.ts
+++
@@ -1,8 +0,0 @@
-import { TextInputHandler } from "./modules/TextInput"
-
-const textInput = document.querySelector("input[type='text'][id='test']") as HTMLInputElement
-let validationClass = textInput.getAttribute('vtype')
-
-textInput.addEventListener('input', (event)=>{
-    TextInputHandler(event)
-})
